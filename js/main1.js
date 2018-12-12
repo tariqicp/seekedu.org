23c81c7d1a5525f231a1a46b6b901a01ad8b8526
dwayne <dwayne@fashiongatekeepers.com>
2018-12-12 17:24:32 -0500

    load more

--- js/main1.js
+++ js/main1.js
@@ -419,58 +419,6 @@
             });
             return false;
         });
-
-        // Isotope Load more button
-
-		var $container = $('.iso-content');
-		var initShow = 8; //number of items loaded on init & onclick load more button
-		var counter = initShow; //counter for load more button
-		var iso = $container.data('isotope'); // get Isotope instance
-
-		loadMore(initShow); //execute function onload
-
-		function loadMore(toShow) {
-			$container.find(".hidden").removeClass("hidden");
-
-			var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
-				return item.element;
-			});
-			$(hiddenElems).addClass('hidden');
-			$container.isotope('layout');
-
-			//when no more to load, hide show more button
-			if (hiddenElems.length == 0) {
-				jQuery("#load-more").hide();
-			} else {
-				jQuery("#load-more").show();
-			};
-
-		}
-
-		//append load more button
-		$container.after('<button id="load-more" class="tem-btn"> Load More</button>');
-
-		//when load more button clicked
-		$("#load-more").click(function() {
-			if ($('.iso-nav li').data('clicked')) {
-				//when filter button clicked, set initial value for counter
-				counter = initShow;
-				$('.iso-nav li').data('clicked', false);
-			} else {
-				counter = counter;
-			};
-
-			counter = counter + initShow;
-
-			loadMore(counter);
-		});
-
-		//when filter button clicked
-		$(".iso-nav li").click(function() {
-			$(this).data('clicked', true);
-
-			loadMore(initShow);
-		});
     });
 
 }(jQuery));
